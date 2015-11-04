ea46c037ad6ca432532ce8ffe53950ff03c12102
lucia1bossoni <boss.lucia@gmail.com>
2015-11-04 15:20:23 +0000

    Modofied function that simulates a noisy gaussian set of data

--- new_sim.py
+++ new_sim.py
@@ -2,20 +2,17 @@
 Specify the start,stop and number of points of the independent variable, as wel as the mean and variance of the gaussian 
 """
 
-
+import numpy as np
+from scipy.stats import norm
+%matplotlib inline
+import matplotlib.pyplot as plt
+import scipy.optimize as optimization
 
 #simulates a gaussian with random noise
 
-def fake_gauss(np.linspace(start=-10, stop=10, num=100),mu=1,sig=1):
+def data(start=-10, stop=10, num=100,mu=1,sig=1):
     x=np.linspace(start=-10, stop=10, num=100)
-    fake_data=np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+    gaussian=np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 
-    fake_data_n=fake_data+0.2*np.random.randn(len(x))  #add noise to data
-
-
-
-    #plot data
-    plt.plot(x,fake_data_n)
-
-#np.show()
+    return data=gaussian+0.2*np.random.randn(len(x))  #add noise to data
